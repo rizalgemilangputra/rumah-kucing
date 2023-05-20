a92b66505f0bbd38a2809e5624836adefba39840
rizalgemilangputra <rizal.gemilangputra@gmail.com>
2023-05-21 01:43:48 +0700

    initial commit

---
+++ javascript/cats.js
@@ -0,0 +1,289 @@
+var data = [
+    {
+        "name": "Kucing abisinia",
+        "file": "ras-kucing-abisinia.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Bali",
+        "file": "ras-kucing-bali.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Birman",
+        "file": "ras-kucing-birman.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing British Shorthair",
+        "file": "ras-kucing-british-shorthair.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Burma",
+        "file": "ras-kucing-burma.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Burmilla",
+        "file": "ras-kucing-burmilla.jpg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Siamese",
+        "file": "ras-kucing-siamese.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Ragdoll",
+        "file": "ras-kucing-ragdoll.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Persia",
+        "file": "ras-kucing-persia.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Manie Coon",
+        "file": "ras-kucing-manie-coon.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Bengal",
+        "file": "ras-kucing-bengal.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing American Shorthair",
+        "file": "ras-kucing-american-shorthair.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Chinchilla",
+        "file": "ras-kucing-chinchilla.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Cornish Rex",
+        "file": "ras-kucing-cornish-rex.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Devon Rex",
+        "file": "ras-kucing-devon-rex.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Moggie",
+        "file": "ras-kucing-moggie.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Exotic Shorthair",
+        "file": "ras-kucing-exotic-shorthari.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Exotic Bobtail",
+        "file": "ras-kucing-bobtail.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Korat",
+        "file": "ras-kucing-korat.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Manx",
+        "file": "ras-kucing-manx.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Hutan Norwegia",
+        "file": "ras-kucing-hutan-norwegia.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Hutan Ocicat",
+        "file": "ras-kucing-ocicat.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Oriental Shorthair",
+        "file": "ras-kucing-oriental-shorthair.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Biru Rusia",
+        "file": "ras-kucing-biru-rusia.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Singapura",
+        "file": "ras-kucing-singapura.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Scottish",
+        "file": "ras-kucing-scottish-fold.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Siberia",
+        "file": "ras-kucing-siberia.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Snowshoe",
+        "file": "ras-kucing-snowshoe.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Somali",
+        "file": "ras-kucing-somali.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Sphinx",
+        "file": "ras-kucing-sphinx.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Tiffanie",
+        "file": "ras-kucing-tiffanie.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Tonkinese",
+        "file": "ras-kucing-tonkinese.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+    {
+        "name": "Kucing Turkish Van",
+        "file": "ras-kucing-turkish-van.jpeg",
+        "wight": "Berat 8 sampai 15 pon",
+        "tall": "Tinggi 8 sampai 10 inci",
+        "age": "Umur 15 sampai 20 tahun",
+        "desc": "Umur 15 sampai 20 tahun",
+    },
+];
+
+var html = '';
+var margin = 0;
+for (var i = 0; i < data.length; i++) {
+    if (i % 2 == 0) {
+        margin = 25;
+    } else {
+        margin = 0;
+    }
+    html += `<div class="item-cat" style="margin-right: ${margin}px">`;
+    html += `<img src="assets/img/cats/${data[i].file}" alt="${data[i].file}">`;
+    html += '<div class="description">';
+    html += `<h3>${data[i].name}</h3>`;
+    html += '<ul>';
+    html += '<li>Berat 8 sampai 15 pon</li>';
+    html += '<li>Tinggi 8 sampai 10 inci</li>';
+    html += '<li>Umur 15 sampai 20 tahun</li>';
+    html += '<li>Mantel bulu pendek, mantel bulu yang berkilau <br> dengan corak warna yang khas.</li>';
+    html += '</ul>';
+    html += '</div>';
+    html += '</div>';
+}
+document.getElementById("cats").insertAdjacentHTML('beforeend', html);
